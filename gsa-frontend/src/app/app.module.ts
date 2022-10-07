4de9cba535b96cab27ae987b81472aed2e3fb64e
Addimator <adpri100hhu.de>
2022-10-07 09:32:30 +0100

    Set up angular project

---
+++ gsa-frontend/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {HttpClientModule} from "@angular/common/http";
+import {AnalysisToolsComponent} from "./analysis-tools/analysis-tools.component";
+
+@NgModule({
+  declarations: [
+    AppComponent, AnalysisToolsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
