3974eac946fc5ba12a1f4e83a6e9692f3d08a025
Addimator <adpri100hhu.de>
2022-10-11 13:22:57 +0100

    Add selection of dataset

---
+++ gsa-frontend/src/app/gsa-form/select-dataset/select-dataset.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gsa-select-dataset',
+  templateUrl: './select-dataset.component.html',
+  styleUrls: ['./select-dataset.component.scss']
+})
+export class SelectDatasetComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
