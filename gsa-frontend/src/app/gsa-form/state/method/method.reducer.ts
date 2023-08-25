4b8dbd004e6c2d366b2de7792b0ad068cd6ecaa9
cqgong <cgong@ebi.ac.uk>
2023-08-25 15:31:41 +0100

    :sparkles: include sample groups parameter

--- gsa-frontend/src/app/gsa-form/state/method/method.reducer.ts
+++ gsa-frontend/src/app/gsa-form/state/method/method.reducer.ts
@@ -1,13 +1,10 @@
 import {ActionReducer, createReducer, on} from "@ngrx/store";
 import {initialState, methodAdapter, MethodState} from "./method.state";
 import {methodActions} from "./method.action";
-import {parameterActions} from "../parameter/parameter.action";
-import {parameterAdapter} from "../parameter/parameter.state";
-
 
 export const methodReducer: ActionReducer<MethodState> = createReducer(
     initialState,
-    on(methodActions.loadSuccess, (state, {methods}) => methodAdapter.addMany(methods.map(method => ({...method, parameters: method.parameters.filter(param => param.name !== 'sample_groups')})), state)),
+    on(methodActions.loadSuccess, (state, {methods}) => methodAdapter.addMany(methods.map(method => ({...method, parameters: method.parameters})), state)),
     on(methodActions.update, (state, {update}) => methodAdapter.updateOne(update, state)),
     on(methodActions.setParams, (state, {methodName, parameters}) => methodAdapter.updateOne({
         id: methodName,
