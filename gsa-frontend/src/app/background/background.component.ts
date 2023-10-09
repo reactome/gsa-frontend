31093732883335e5646895d3ca918125c3c28d6f
EliotRagueneau <eragueneau@ebi.ac.uk>
2023-10-09 10:13:59 +0100

    :art: Add component dedicated to the background

---
+++ gsa-frontend/src/app/background/background.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gsa-background',
+  templateUrl: './background.component.html',
+  styleUrls: ['./background.component.scss']
+})
+export class BackgroundComponent {
+
+}
