0730dcd93db493845d605390f33bd3ceb233dbb6
EliotRagueneau <eragueneau@ebi.ac.uk>
2024-02-16 17:39:55 +0000

    :sparkles: Include more details on mail subject from errors

--- gsa-frontend/src/app/gsa-form/analysis/analysis.component.ts
+++ gsa-frontend/src/app/gsa-form/analysis/analysis.component.ts
@@ -2,7 +2,11 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Store} from "@ngrx/store";
 import {analysisFeature} from "../state/analysis/analysis.selector";
-import {CdkStep} from "@angular/cdk/stepper";
+import {formatDate} from "@angular/common";
+import {methodFeature} from "../state/method/method.selector";
+import {combineLatest, map} from "rxjs";
+import {datasetFeature} from "../state/dataset/dataset.selector";
+import {isDefined} from "../utilities/utils";
 
 
 @Component({
@@ -16,6 +20,20 @@ export class AnalysisComponent implements OnInit {
   analysisLoadingStatus$ = this.store.select(analysisFeature.selectAnalysisLoadingStatus)
   reportLoadingStatus$ = this.store.select(analysisFeature.selectReportLoadingStatus)
   result$ = this.store.select(analysisFeature.selectAnalysisResult)
+
+  statusString$ = combineLatest([
+    this.store.select(methodFeature.selectSelectedMethod),
+    this.store.select(datasetFeature.selectEntities)]
+  ).pipe(
+      map(([method, datasets]) => {
+        const datasetString = Object.values(datasets)
+          .filter(isDefined)
+          .filter(dataset => dataset.saved)
+          .map(dataset => `${dataset.summary?.title} - ${dataset.statisticalDesign?.comparisonGroup1} vs ${dataset.statisticalDesign?.comparisonGroup2}`)
+          .join(" | ")
+        return `${method?.name} | ${datasetString}`
+      })
+  )
 
   @Input() datasetId: number;
   @Output() restart = new EventEmitter<void>()
@@ -33,5 +51,9 @@ export class AnalysisComponent implements OnInit {
 
   goToSelectMethod() {
     this.restart.emit()
+  }
+
+  mail(statusString: string | null): string {
+    return `mailto:help@reactome.org?subject=${encodeURIComponent(`Reactome GSA failed [${statusString || 'No status'}] on ${formatDate(new Date(), 'short', 'en-US')}`)}`
   }
 }
