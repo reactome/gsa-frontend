6272e75f69286a000241827b6cb426ae733c8161
Eliot Ragueneau <eragueneau@ebi.ac.uk>
2025-07-24 01:15:51 +0200

    :arrow_up: Angular 17

--- gsa-frontend/src/app/app.module.ts
+++ gsa-frontend/src/app/app.module.ts
@@ -46,6 +46,7 @@ import {FormsModule} from "@angular/forms";
       autoPause: true, // Pauses recording actions and state changes when the extension window is not open
       trace: true, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
       traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+      connectInZone: true
     }),
     EffectsModule.forRoot([]),
     StoreRouterConnectingModule.forRoot(),
